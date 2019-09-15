0be4179aebce2a33f1125fe46ed000b2f4351e98
Dav991123 <sargsyand89@gmail.com>
2019-09-16 01:45:03 +0400

    aa

---
+++ build/precache-manifest.b114dd7877dc878b7e0e09b463fdc92b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a6283559a9e9e81787ea4e7fb6c919",
+    "url": "/react-hooks-cryptocurrency/index.html"
+  },
+  {
+    "revision": "a7660e2e517e64c88f92",
+    "url": "/react-hooks-cryptocurrency/static/css/main.5ea1a0ea.chunk.css"
+  },
+  {
+    "revision": "ae4fe41523dbccc1a34d",
+    "url": "/react-hooks-cryptocurrency/static/js/2.fd7015ad.chunk.js"
+  },
+  {
+    "revision": "a7660e2e517e64c88f92",
+    "url": "/react-hooks-cryptocurrency/static/js/main.42bfff10.chunk.js"
+  },
+  {
+    "revision": "8eb2b7ca4af89e49b7dc",
+    "url": "/react-hooks-cryptocurrency/static/js/runtime~main.9306ec2f.js"
+  },
+  {
+    "revision": "5c0436d55f12af99760ca65640d19d19",
+    "url": "/react-hooks-cryptocurrency/static/media/logo.5c0436d5.png"
+  }
+]);
